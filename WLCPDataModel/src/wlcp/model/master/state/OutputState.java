bd09d70c47aa6e9931144a2e1188558f581ee9dd
mmicciolo <mmicciolo@wpi.edu>
2017-12-19 19:46:57 -0500

    Few Cleanups

--- WLCPDataModel/src/wlcp/model/master/state/OutputState.java
+++ WLCPDataModel/src/wlcp/model/master/state/OutputState.java
@@ -1,9 +1,7 @@
 package wlcp.model.master.state;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.persistence.*;

b4de483786c62277fd8d4d1ee472c28e40a79530
mmicciolo <mmicciolo@wpi.edu>
2018-03-14 13:06:05 -0400

    Game Instance Starting.

--- WLCPFrontEnd/WebContent/wlcpfrontend/controllers/GameInstances.controller.js
+++ WLCPFrontEnd/WebContent/wlcpfrontend/controllers/GameInstances.controller.js
@@ -1,13 +1,140 @@
 sap.ui.controller("wlcpfrontend.controllers.GameInstances", {
 
+	socket : null,
+	
+	onStartGameInstance : function() {
+		
+		//Create an instance of the dialog
+		this.dialog = sap.ui.xmlfragment("wlcpfrontend.fragments.GameInstances.StartGameInstance", this);
+		
+		//Set the model
+		this.dialog.setModel(ODataModel.getODataModel(), "odata");
+		
+		//Setup an on after rendering function for filtering
+		this.dialog.addEventDelegate({
+			onAfterRendering : $.proxy(this.onAfterRenderingStartGameInstance, this)
+		});
+		
+		//Open the dialog
+		this.dialog.open();
+	},
+	
+	onAfterRenderingStartGameInstance : function () {
+		var gameBinding = sap.ui.getCore().byId("gameInstanceGame").getBinding("items");
+		gameBinding.filter([new sap.ui.model.Filter("Username", "EQ", sap.ui.getCore().getModel("user").oData.username)]);
+		var gameLobbyBinding = sap.ui.getCore().byId("gameInstanceGameLobby").getBinding("items");
+		gameLobbyBinding.filter([new sap.ui.model.Filter("Username", "EQ", sap.ui.getCore().getModel("user").oData.username)]);
+	},
+	
+	onCancel : function(oEvent) {
+		this.dialog.close();
+		this.dialog.destroy();
+	},
+	
+	onStopGameInstance : function() {
+		sap.m.MessageBox.confirm("Are you sure you want to stop this game instance?", {onClose : $.proxy(this.onDeleteConfirm, this)});
+	},
+	
+	onEdit : function() {
+		var oTileContainer = this.getView().byId("gameLobbyTileContainer");
+		var newValue = !oTileContainer.getEditable();
+		oTileContainer.setEditable(newValue);
+	},
+	
+	startGameInstance : function() {
+		this.busy = new sap.m.BusyDialog();
+		this.busy.open();
+		this.socket = new WebSocket('ws://24.240.135.66:3333');
+		this.socket.binaryType = "arraybuffer";
+		this.socket.onopen = $.proxy(this.onOpen, this);
+		this.socket.onmessage = $.proxy(this.onMessage, this);
+	},
+	
+	onOpen : function(event) {
+		var gameId = sap.ui.getCore().byId("gameInstanceGame").getSelectedKey();
+		var gameLobbyId = sap.ui.getCore().byId("gameInstanceGameLobby").getSelectedKey();
+//		if(gameId == "" || gameLobbyId == "") {
+//			this.gameStartError();
+//			return;
+//		}
+		var byteBuffer = new dcodeIO.ByteBuffer();
+		byteBuffer.writeByte(0);
+		byteBuffer.writeInt(0);
+		byteBuffer.writeInt(gameId.length);
+		byteBuffer.writeString(gameId);
+		if(isNaN(parseInt(gameLobbyId))) { 
+			byteBuffer.writeInt(-1); 
+		} else {
+			byteBuffer.writeInt(parseInt(gameLobbyId));
+		}
+		byteBuffer.writeInt(sap.ui.getCore().getModel("user").oData.username.length);
+		byteBuffer.writeString(sap.ui.getCore().getModel("user").oData.username);
+		byteBuffer.writeInt(byteBuffer.offset, 1);
+		byteBuffer.flip();
+		var buffer = byteBuffer.toArrayBuffer();
+		this.socket.send(buffer);
+	},
+	
+	onMessage : function(event) {
+		var byteBuffer = new dcodeIO.ByteBuffer();
+		byteBuffer.append(new Uint8Array(event.data));
+		byteBuffer.flip();
+		switch(byteBuffer.readByte()) {
+		case 2:
+			byteBuffer.readInt();
+			this.gameStartError(byteBuffer.readInt());
+			break;
+		case 14:
+			this.gameStartedSuccess();
+			break;
+		default:
+			break;
+		}
+	},
+	
+	gameStartedSuccess : function() {
+		this.socket.close();
+		this.onCancel();
+		this.busy.close();
+		ODataModel.getODataModel().refresh();
+		sap.m.MessageToast.show("Game Instance Start Successfully!");
+	},
+	
+	gameStartError : function(errorCode) {
+		switch(errorCode) {
+		case 0:
+			sap.m.MessageToast.show("Selected Game Does Not Exist!");
+			break;
+		case 1:
+			sap.m.MessageToast.show("Selected Game Lobby Does Not Exist!");
+			break;
+		case 2:
+			sap.m.MessageToast.show("Username Does Not Exist!");
+			break;
+		case 3:
+			sap.m.MessageToast.show("Game Instance Has Already Been Started!");
+			break;
+		default:
+			break;
+		}
+		this.socket.close();
+		this.onCancel();
+		this.busy.close();
+	},
+	
 /**
 * Called when a controller is instantiated and its View controls (if available) are already created.
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 * @memberOf wlcpfrontend.views.GameInstances
 */
-//	onInit: function() {
-//
-//	},
+	onInit: function() {
+		this.getView().byId("gameInstanceTileContainer").addEventDelegate({
+			  onAfterRendering: function(){
+			        var oBinding = this.getView().byId("gameInstanceTileContainer").getBinding("tiles");
+			        oBinding.filter([new sap.ui.model.Filter("Username", "EQ", sap.ui.getCore().getModel("user").oData.username)]);
+			  }
+			}, this);
+	},
 
 /**
 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered

b8b131c5e97c823db2af190395efcb25dce6a4f9
mmicciolo <mmicciolo@wpi.edu>
2018-07-13 20:46:41 -0400

    Scope Change test cases.

--- WLCPFrontEnd/WebContent/wlcpfrontend/jasmine/spec/ValidationEngineScopeChangeSpec.js
+++ WLCPFrontEnd/WebContent/wlcpfrontend/jasmine/spec/ValidationEngineScopeChangeSpec.js
@@ -58,7 +58,28 @@ describe("A suite to test different scope changes for states & transitions like 
 		expect(outputState.scopeMask == 8188 && outputState2.scopeMask == 114).toBeTruthy();
 	});
 	it("Single Connection No Transition Player Wide -> Team Wide Different States Single Team (3x3)", function() {
+		GameEditorTestingHelpers.resetGameEditor();
 		
+		var startState = GameEditorTestingHelpers.createNewGame(3, 3);
+		var outputState = GameEditorTestingHelpers.addState(500, 250);
+		var outputState2 = GameEditorTestingHelpers.addState(750, 250);
+		var outputState3 = GameEditorTestingHelpers.addState(1000, 250);
+		var outputState4 = GameEditorTestingHelpers.addState(750, 500);
+		var connection = GameEditorTestingHelpers.addConnection(startState.htmlId, outputState.htmlId);
+		var connection2 = GameEditorTestingHelpers.addConnection(startState.htmlId, outputState2.htmlId);
+		var connection3 = GameEditorTestingHelpers.addConnection(startState.htmlId, outputState3.htmlId);
+		var connection4 = GameEditorTestingHelpers.addConnection(outputState.htmlId, outputState4.htmlId);
+		var connection5 = GameEditorTestingHelpers.addConnection(outputState2.htmlId, outputState4.htmlId);
+		var connection6 = GameEditorTestingHelpers.addConnection(outputState3.htmlId, outputState4.htmlId);
+		
+		outputState.modelJSON.iconTabs[4].navigationContainerPages[0].displayText = "Hello World!";
+		outputState.onChange();
+		outputState2.modelJSON.iconTabs[2].navigationContainerPages[0].displayText = "Hello World!";
+		outputState2.onChange();
+		outputState3.modelJSON.iconTabs[2].navigationContainerPages[0].displayText = "Hello World!";
+		outputState3.onChange();
+
+		expect(outputState.scopeMask == 8092 && outputState2.scopeMask == 8108 && outputState3.scopeMask == 8140 && outputState4.scopeMask == 114).toBeTruthy();
 	});
 	it("Single Connection No Transition Team Wide -> Game Wide Same State (3x3)", function() {
 		GameEditorTestingHelpers.resetGameEditor();
@@ -78,7 +99,28 @@ describe("A suite to test different scope changes for states & transitions like 
 		expect(outputState.scopeMask == 14 && outputState2.scopeMask == 8191).toBeTruthy();
 	});
 	it("Single Connection No Transition Team Wide -> Game Wide Different States (3x3)", function() {
+		GameEditorTestingHelpers.resetGameEditor();
+		
+		var startState = GameEditorTestingHelpers.createNewGame(3, 3);
+		var outputState = GameEditorTestingHelpers.addState(500, 250);
+		var outputState2 = GameEditorTestingHelpers.addState(750, 250);
+		var outputState3 = GameEditorTestingHelpers.addState(1000, 250);
+		var outputState4 = GameEditorTestingHelpers.addState(750, 500);
+		var connection = GameEditorTestingHelpers.addConnection(startState.htmlId, outputState.htmlId);
+		var connection2 = GameEditorTestingHelpers.addConnection(startState.htmlId, outputState2.htmlId);
+		var connection3 = GameEditorTestingHelpers.addConnection(startState.htmlId, outputState3.htmlId);
+		var connection4 = GameEditorTestingHelpers.addConnection(outputState.htmlId, outputState4.htmlId);
+		var connection5 = GameEditorTestingHelpers.addConnection(outputState2.htmlId, outputState4.htmlId);
+		var connection6 = GameEditorTestingHelpers.addConnection(outputState3.htmlId, outputState4.htmlId);
+		
+		outputState.modelJSON.iconTabs[1].navigationContainerPages[0].displayText = "Hello World!";
+		outputState.onChange();
+		outputState2.modelJSON.iconTabs[0].navigationContainerPages[0].displayText = "Hello World!";
+		outputState2.onChange();
+		outputState3.modelJSON.iconTabs[0].navigationContainerPages[0].displayText = "Hello World!";
+		outputState3.onChange();
 
+		expect(outputState.scopeMask == 2 && outputState2.scopeMask == 4 && outputState3.scopeMask == 8 && outputState4.scopeMask == 8191).toBeTruthy();
 	});
 	it("Single Connection No Transition Player Wide -> Game Wide Same State (3x3)", function() {
 		GameEditorTestingHelpers.resetGameEditor();
@@ -104,5 +146,62 @@ describe("A suite to test different scope changes for states & transitions like 
 	});
 	it("Single Connection No Transition Player Wide -> Game Wide Different States (3x3)", function() {
 		
+		GameEditorTestingHelpers.resetGameEditor();
+		
+		var startState = GameEditorTestingHelpers.createNewGame(3, 3);
+		var outputState = GameEditorTestingHelpers.addState(0, 250);
+		var outputState2 = GameEditorTestingHelpers.addState(150, 250);
+		var outputState3 = GameEditorTestingHelpers.addState(300, 250);
+		var outputState4 = GameEditorTestingHelpers.addState(450, 250);
+		var outputState5 = GameEditorTestingHelpers.addState(600, 250);
+		var outputState6 = GameEditorTestingHelpers.addState(750, 250);
+		var outputState7 = GameEditorTestingHelpers.addState(900, 250);
+		var outputState8 = GameEditorTestingHelpers.addState(1050, 250);
+		var outputState9 = GameEditorTestingHelpers.addState(1200, 250);
+		var outputState10 = GameEditorTestingHelpers.addState(750, 500);
+		
+		var connection = GameEditorTestingHelpers.addConnection(startState.htmlId, outputState.htmlId);
+		var connection2 = GameEditorTestingHelpers.addConnection(startState.htmlId, outputState2.htmlId);
+		var connection3 = GameEditorTestingHelpers.addConnection(startState.htmlId, outputState3.htmlId);
+		var connection4 = GameEditorTestingHelpers.addConnection(startState.htmlId, outputState4.htmlId);
+		var connection5 = GameEditorTestingHelpers.addConnection(startState.htmlId, outputState5.htmlId);
+		var connection6 = GameEditorTestingHelpers.addConnection(startState.htmlId, outputState6.htmlId);
+		var connection7 = GameEditorTestingHelpers.addConnection(startState.htmlId, outputState7.htmlId);
+		var connection8 = GameEditorTestingHelpers.addConnection(startState.htmlId, outputState8.htmlId);
+		var connection9 = GameEditorTestingHelpers.addConnection(startState.htmlId, outputState9.htmlId);
+
+		var connection10 = GameEditorTestingHelpers.addConnection(outputState.htmlId, outputState10.htmlId);
+		var connection11 = GameEditorTestingHelpers.addConnection(outputState2.htmlId, outputState10.htmlId);
+		var connection12 = GameEditorTestingHelpers.addConnection(outputState3.htmlId, outputState10.htmlId);
+		var connection13 = GameEditorTestingHelpers.addConnection(outputState4.htmlId, outputState10.htmlId);
+		var connection14 = GameEditorTestingHelpers.addConnection(outputState5.htmlId, outputState10.htmlId);
+		var connection15 = GameEditorTestingHelpers.addConnection(outputState6.htmlId, outputState10.htmlId);
+		var connection16 = GameEditorTestingHelpers.addConnection(outputState7.htmlId, outputState10.htmlId);
+		var connection17 = GameEditorTestingHelpers.addConnection(outputState8.htmlId, outputState10.htmlId);
+		var connection18 = GameEditorTestingHelpers.addConnection(outputState9.htmlId, outputState10.htmlId);
+		
+		outputState.modelJSON.iconTabs[4].navigationContainerPages[0].displayText = "Hello World!";
+		outputState.onChange();
+		outputState2.modelJSON.iconTabs[2].navigationContainerPages[0].displayText = "Hello World!";
+		outputState2.onChange();
+		outputState3.modelJSON.iconTabs[2].navigationContainerPages[0].displayText = "Hello World!";
+		outputState3.onChange();
+		outputState4.modelJSON.iconTabs[2].navigationContainerPages[0].displayText = "Hello World!";
+		outputState4.onChange();
+		outputState5.modelJSON.iconTabs[1].navigationContainerPages[0].displayText = "Hello World!";
+		outputState5.onChange();
+		outputState6.modelJSON.iconTabs[1].navigationContainerPages[0].displayText = "Hello World!";
+		outputState6.onChange();
+		outputState7.modelJSON.iconTabs[1].navigationContainerPages[0].displayText = "Hello World!";
+		outputState7.onChange();
+		outputState8.modelJSON.iconTabs[0].navigationContainerPages[0].displayText = "Hello World!";
+		outputState8.onChange();
+		outputState9.modelJSON.iconTabs[0].navigationContainerPages[0].displayText = "Hello World!";
+		outputState9.onChange();
+
+		expect(outputState.scopeMask == 16 && outputState2.scopeMask == 32 && outputState3.scopeMask == 64 && 
+			   outputState4.scopeMask == 128 && outputState5.scopeMask == 256 && outputState6.scopeMask == 512 &&
+			   outputState7.scopeMask == 1024 && outputState8.scopeMask == 2048 && outputState9.scopeMask == 4096 &&
+			   outputState10.scopeMask == 8191).toBeTruthy();
 	});
 });

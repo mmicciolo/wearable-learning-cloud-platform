933b582051c83618040565d8e614b1030dff71b7
mmicciolo <mmicciolo@wpi.edu>
2018-10-03 21:39:55 -0400

    Fixed default server config.

--- WLCPFrontEnd/WebContent/wlcpfrontend/javascript/ServerConfig.js
+++ WLCPFrontEnd/WebContent/wlcpfrontend/javascript/ServerConfig.js
@@ -1,7 +1,7 @@
 var ServerConfig = {
 	
 	getServerAddress : function() {
-		//return "130.215.45.83:3333";
-		return "24.240.135.66:3333";
+		return "130.215.45.83:3333";
+		//return "24.240.135.66:3333";
 	}
 }

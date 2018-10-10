1fd7e07b3c32553cadc80dbb2e09d609c39284ab
mmicciolo <mmicciolo@wpi.edu>
2018-10-10 19:07:34 -0400

    Fixed bug with JPA + Updated Project Settings.
    Transition Sequence + Keyboard didnt have ManyToOne relationship on
    other end.

--- WLCPDataModel/src/main/java/wlcp/model/master/transition/KeyboardInput.java
+++ WLCPDataModel/src/main/java/wlcp/model/master/transition/KeyboardInput.java
@@ -12,6 +12,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -33,6 +34,7 @@ public class KeyboardInput implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer keyboardInputId;
 	
+	@ManyToOne
 	@JoinColumn(name = "TRANSITION_ID")
 	@JsonIgnoreProperties(value= {"game", "connection", "activeTransitions", "singleButtonPresses", "sequenceButtonPresses", "keyboardInputs"})
 	private Transition transition;

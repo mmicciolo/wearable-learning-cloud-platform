ee2cf76f4dca0efb57d8a38002719068430fb8a8
mmicciolo <mmicciolo@wpi.edu>
2019-01-14 21:44:01 -0500

    Updated ServerConfig with wpi ip.

--- WLCPFrontEnd/WebContent/wlcpfrontend/javascript/ServerConfig.js
+++ WLCPFrontEnd/WebContent/wlcpfrontend/javascript/ServerConfig.js
@@ -1,7 +1,7 @@
 var ServerConfig = {
 	
 	getServerAddress : function() {
-		//return "130.215.45.83:3333";
-		return "localhost:3333";
+		return "130.215.45.83:3333";
+		//return "localhost:3333";
 	}
 }
